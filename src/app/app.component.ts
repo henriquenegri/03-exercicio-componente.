6eae167ef675fe445cc32fd5f767cbce5866bb52
Henrique De Almeida Negri <riquenegri11@gmail.com>
2025-03-11 19:22:21 -0300

    finalizado os exercí

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeiroComponenteComponent } from "./primeiro-componente/primeiro-componente.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PrimeiroComponenteComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
